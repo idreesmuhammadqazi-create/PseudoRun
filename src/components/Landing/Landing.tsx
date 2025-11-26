a6f7ad76170f77c327f761d326e4e3b73460d15e
idrees qazi <idreesmuhammadqazi@gmail.com>
2025-11-26 16:19:58 +0500

    Update Landing.tsx

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -163,9 +163,9 @@ export default function Landing() {
              alt="Launch List Badge" />
         </a>
         <a href="https://turbo0.com/item/pseudorun" target="_blank" rel="noopener noreferrer">
-  {/* @ts-expect-error - Bot verification requires HTML-style attributes */}
-  <img src="https://img.turbo0.com/badge-listed-light.svg" alt="Listed on Turbo0" style="height: 54px; width: auto;" />
-</a>
+          {/* @ts-expect-error - Bot verification requires HTML-style attributes */}
+          <img src="https://img.turbo0.com/badge-listed-light.svg" alt="Listed on Turbo0" style="height: 54px; width: auto;" />
+        </a>
         <a href="https://submithunt.com" target="_blank" rel="noopener noreferrer">
           <img src="https://submithunt.com/badge.png" alt="Featured on SubmitHunt" width="150" height="45" />
         </a>
