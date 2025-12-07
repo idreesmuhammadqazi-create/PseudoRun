7d86a5e67634f6bb256fe2764e5894c1063efd1b
idrees qazi <idreesmuhammadqazi@gmail.com>
2025-12-07 20:04:34 +0500

    Add new external links to Landing component

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -182,6 +182,8 @@ export default function Landing() {
         <a target="_blank" href="https://productlistdir.com"><img src="https://productlistdir.com/assets/images/badge.png" alt="Product List Dir" height="54"/></a>
         <a target="_blank" href="https://milliondothomepage.com"><img src="https://milliondothomepage.com/assets/images/badge.png" alt="Million Dot Homepage" height="54"/></a>
         <a target="_blank" href="https://launchclash.com"><img src="https://launchclash.com/static/images/badge.png" alt="LaunchClash" height="54"/></a>
+        <a target="_blank" href="https://shinylaunch.com"><img src="https://shinylaunch.com/static/images/badge.png" alt="ShinyLaunch" height="54"/></a>
+        <a target="_blank" href="https://acidtools.com"><img src="https://acidtools.com/assets/images/badge.png" alt="Acid Tools" height="54"/></a>
 
       </footer>
       
